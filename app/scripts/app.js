ea99821d8969da9095d40a1fd348852b3bd206cc
JBigs206 <JeffreyRichardBigler@gmail.com>
2016-04-28 14:23:44 -0700

    Bootstraped App Final

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wats2000FinalApp
+ * @description
+ * # wats2000FinalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wats2000FinalApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
