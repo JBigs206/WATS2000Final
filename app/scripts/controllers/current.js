58d707bd54424fd4e28a63831f43b3a2d0297858
JBigs206 <JeffreyRichardBigler@gmail.com>
2016-05-22 22:31:48 -0700

    Added transitions to app

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -9,9 +9,35 @@
  */
 angular.module('wats2000FinalApp')
   .controller('CurrentCtrl', function ($scope, $routeParams, current, $localStorage) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    $scope.saveCity = function(city){
+    var cityData = {
+        'name': game.title,
+        'id': game.thumb
+    };
+    if (!$localStorage.savedCities){
+        $localStorage.savedCities = [cityData];
+    } else {
+        // Check to make sure we haven't already saved the city.
+        var save = true;
+        for (var i=0; i < $localStorage.savedCities.length; i++){
+            if ($localStorage.savedCities[i].id === cityData.id) {
+                // this is a duplicate, so don't save
+                save = false;
+            }
+        }
+        if (save===true){
+            $localStorage.savedCities.push(cityData);
+            // Add object to trigger messages
+            $scope.citySaved = {
+                'success': true
+            };
+        } else {
+            console.log('city already saved');
+            // Add object to trigger messages
+            $scope.citySaved = {
+                'duplicate': true
+            };
+        }
+    }
+}
+});
